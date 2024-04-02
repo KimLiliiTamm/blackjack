07f46a03ae70921653aa6d4cfa51050db7051388
hirv7 <165573091+hirv7@users.noreply.github.com>
2024-04-03 00:49:28 +0300

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
-    //siia tuleb mängija klass
+    
+    private List<Kaart> kaardid;
+    private int kaartideSumma;
+
+    public Player(List<Kaart> kaardid, int kaartideSumma) {
+        this.kaardid = kaardid;
+        this.kaartideSumma = kaartideSumma;
+    }
+
+    public void lisaKaart(Kaart kaart) {
+        kaardid.add(kaart);
+    }
+
+    public int getKaartideSumma() {
+        return kaartideSumma;
+    }
+
+    public List<Kaart> getKaardid() {
+        return kaardid;
+    }
 }
