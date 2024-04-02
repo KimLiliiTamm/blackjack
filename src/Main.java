bee5c1826b03666037271bac7d5628855d19c486
hirv7 <165573091+hirv7@users.noreply.github.com>
2024-04-03 00:48:08 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,22 @@
 public class Main {
-    //siia tuleb blackjacki main funktioon
+    public static Kaardipakk kaardipakiLoomine() {
+        java.lang.String kaartideKogu = "2♥,3♥,4♥,5♥,6♥,7♥,8♥,9♥,10♥,J♥,Q♥,K♥,A♥," +
+                "2♦,3♦,4♦,5♦,6♦,7♦,8♦,9♦,10♦,J♦,Q♦,K♦,A♦," +
+                "2♣,3♣,4♣,5♣,6♣,7♣,8♣,9♣,10♣,J♣,Q♣,K♣,A♣," +
+                "2♠,3♠,4♠,5♠,6♠,7♠,8♠,9♠,10♠,J♠,Q♠,K♠,A♠";
+        List<Kaart> kaardid= new ArrayList<>();
+        String[] tükid = kaartideKogu.split(",");
+        int j = 0; // Kuna piltidega kaartidel tuleb väärtuseks 10 ja ässal 11 ning 1, siis paremat meetodit järje pidamiseks ei suutnud välja mõelda xd
+        for (int i = 0; i < 52; i++, j++) {
+            // TODO
+        }
+        Kaardipakk kaardipakk = new Kaardipakk(kaardid);
+
+        return kaardipakk;
+    }
+
+    public static void main(String[] args) {
+        Kaardipakk kaardipakk = kaardipakiLoomine();
+        // TODO
+    }
 }
